33ec99e24c107544b5ca90f0ae15e76b76dd2f1b
Beñat Aranzadi Duran <aranzadi99@gmail.com>
2025-11-27 13:31:36 +0100

    Lehenengo commit-a (Login eta Datu Base konexioa)

---
+++ TPV/DB Konexioa.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TPV
+{
+
+    public class DB_Konexioa
+    {
+        private readonly string connectionString =
+            "Server=192.168.115.153;Database=jatetxea;Uid=admin;Pwd=abc123ABC@;";
+
+
+        public MySqlConnection Conexion { get; }
+
+        public DB_Konexioa()
+        {
+            Conexion = new MySqlConnection(connectionString);
+        }
+    }
+}
